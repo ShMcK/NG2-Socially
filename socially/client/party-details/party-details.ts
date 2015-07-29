93d9ca8987dbbeb73eda4d0b97d4dffd93d36e47
ShMcK <me@shmck.com>
2015-07-29 14:59:31 +0900

    step 07 init

--- socially/client/party-details/party-details.ts
+++ socially/client/party-details/party-details.ts
@@ -13,9 +13,28 @@ export class PartyDetails {
   constructor(@Inject(RouteParams) routeParams:RouteParams) {
     this.partyId = routeParams.params.partyId;
   }
+
+  add(party) {
+
+    if (party.name.length && party.description.length) {
+      // insert parties (insecure way)
+      Parties.update(party._id, {
+        name: party.name,
+        description: party.description
+      });
+
+      this.reset();
+    }
+  }
+
+  reset() {
+    this.party = this.originalParty;
+  }
+
   onActivate() {
     this.party = Parties.find(this.partyId).fetch()[0];
     if (this.party) {
+      this.originalParty = this.party;
       return true;
     }
   }
