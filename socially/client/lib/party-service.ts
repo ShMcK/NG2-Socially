fb17e2a9585af7b99419bbebfc800f3e07d88227
ShMcK <me@shmck.com>
2015-07-29 21:07:38 +0900

    party-service refactor

--- socially/client/lib/party-service.ts
+++ socially/client/lib/party-service.ts
@@ -4,22 +4,22 @@ export class PartyService {
   // note: these methods are still client-side and thus insecure
 
   add(party:IParty) {
-    Parties.insert({
-      name: party.name,
-      description: party.description,
-      user: Meteor.userId()
-    });
+    Parties.insert(this.getPartyObject(party));
   }
 
   update(party:IParty) {
-    Parties.update(party._id, {
-      name: party.name,
-      description: party.description,
-      user: Meteor.userId()
-    });
+    Parties.update(party._id, this.getPartyObject(party));
   }
 
   remove(partyId:string) {
     Parties.remove(partyId);
+  }
+
+  getPartyObject(party) {
+    return {
+      name: party.name,
+      description: party.description,
+      user: Meteor.userId()
+    };
   }
 }
