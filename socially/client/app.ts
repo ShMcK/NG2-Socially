bcdd82271f5c1c1d8fee6c1f3bf12e9ba9c41a77
ShMcK <me@shmck.com>
2015-07-29 10:33:25 +0900

    step 00 complete

---
+++ socially/client/app.ts
@@ -0,0 +1,14 @@
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+  selector: 'app'
+})
+@View({
+  templateUrl: 'client/index.ng.html'
+})
+class Socially {
+}
+
+bootstrap(Socially);
+
+
