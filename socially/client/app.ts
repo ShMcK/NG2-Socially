4495ccb1da00e84c44b952441ebd42243bf66276
ShMcK <me@shmck.com>
2015-07-29 10:48:17 +0900

    step-02

--- socially/client/app.ts
+++ socially/client/app.ts
@@ -1,12 +1,23 @@
-import {Component, View, bootstrap} from 'angular2/angular2';
+import {Component, View, bootstrap, NgFor} from 'angular2/angular2';
 
 @Component({
   selector: 'app'
 })
 @View({
-  templateUrl: 'client/index.ng.html'
+  templateUrl: 'client/index.ng.html',
+  directives: [NgFor]
 })
 class Socially {
+  constructor () {
+    this.parties = [
+      {'name': 'Dubstep-Free Zone',
+        'description': 'Can we please just for an evening not listen to dubstep.'},
+      {'name': 'All dubstep all the time',
+        'description': 'Get it on!'},
+      {'name': 'Savage lounging',
+        'description': 'Leisure suit required. And only fiercest manners.'}
+    ];
+  }
 }
 
 bootstrap(Socially);
